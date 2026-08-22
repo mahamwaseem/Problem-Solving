a461cf38970a7585fb1c8680b7e06a23b402a9b8
Maham Waseem <mahamw440@gmail.com>
2026-08-22 19:57:49 +0500

    Input two numbers from user

---
+++ arithmeticop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+   int a, b;
+
+  cout<< "Enter 2 numbers: "<< endl;
+  cin>> a ;
+ 
+  cin >> b ;
+   
+  cout<<"Two number are " << a << " and " << b;
+  return 0;
+}
