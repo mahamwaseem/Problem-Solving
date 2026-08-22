5fe6980e3eed139e0ff4a9de200182a698de7f35
Maham Waseem <mahamw440@gmail.com>
2026-08-22 20:14:46 +0500

    sum, difference, multiplication and division of two numbers

--- arithmeticop.cpp
+++ arithmeticop.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 int main() {
    int a, b;
-
+   
+ 
   cout<< "Enter 2 numbers: "<< endl;
   cin>> a ;
  
   cin >> b ;
    
-  cout<<"Two number are " << a << " and " << b;
+  cout<<"Two number are " << a << " and " << b << endl;
+  cout<< "Sum = " << a+b << endl;
+  cout<< "multiply = " << a*b << endl;
+  cout<< "div = " << a/b << endl;
+  cout<< "diff = " << a-b << endl;
+  
   return 0;
 }
