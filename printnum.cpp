643bc7ac20f887cc2fd7eb2a9bd52bf635a4c31c
Maham Waseem <mahamw440@gmail.com>
2026-08-22 20:48:57 +0500

    print numbers from 1 to n

---
+++ printnum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int num, i;
+
+  cout<<"Enter a number: ";
+  cin>> num;
+
+  for (int i = 1; i<= num; i++){
+    cout<<i<< endl;
+  }
+ 
+  
+  return 0;
+}
