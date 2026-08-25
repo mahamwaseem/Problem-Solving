8b228585b523875269dd97443517e074ee9dece9
Maham Waseem <mahamw440@gmail.com>
2026-08-25 16:02:01 +0500

    find hcf/gcd

---
+++ HCF.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    int gcd = 1;
+
+    for(int i = 1; i <= num1 && i <= num2; i++){
+        if(num1 % i == 0 && num2 % i == 0){
+            gcd = i;
+        }
+    }
+
+    cout << "GCD/HCF: " << gcd;
+
+    return 0;
+}
