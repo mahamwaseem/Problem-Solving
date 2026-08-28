2e24501082fc28cb9d750b2a982679c0fc264d63
Maham Waseem <mahamw440@gmail.com>
2026-08-28 14:58:48 +0500

    reverse a string

---
+++ strings/reverse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+  string a;
+  cout<< "enter a srting: ";
+  cin>> a;
+
+
+  for(int i = a.length(); i>=0; i--){
+    cout<<a[i];
+  }
+  return 0;
+}
